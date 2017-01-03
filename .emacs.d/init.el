62b66398c8567b2f416b5d509511c9d655b4407a
mathwiz <yohanlee@yahoo.com>
2017-01-03 09:59:03 -0500

    change init to linux

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,10 +7,8 @@
 (package-initialize) ;; You might already have this line
 
 (setq inferior-lisp-program
-  "C:\\Software\\clisp-2.49\\full\\lisp.exe -B C:\\Software\\clisp-2.49\\full
--M C:\\Software\\clisp-2.49\\full\\lispinit.mem
--ansi -q")
-(add-to-list 'load-path "C:\\Users\\Yohan\\Documents\\.emacs.d\\elpa\\slime-20150102.1206")
+"/usr/bin/clisp")
+(add-to-list 'load-path "~/.emacs.d/elpa/slime-20150102.1206")
 (require 'slime)
 (require 'slime-autoloads)
 ;; Also setup the slime-fancy contrib
