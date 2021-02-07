70b98348b7e7678e95ba9d8bb267a4a44839c71a
Yohan Lee <yohanlee@yahoo.com>
2021-02-06 20:19:45 -0500

    python interpreter

--- .emacs.d/customizations/setup-languages.el
+++ .emacs.d/customizations/setup-languages.el
@@ -1,5 +1,18 @@
+;; C
+
 (fset 'int-main
    (kmacro-lambda-form [?i ?n ?t ?  ?m ?a ?i ?n ?\( ?v ?o ?i ?d ?\) ?  ?\{ ?\C-m ?\C-m ?\C-i ?r ?e ?t ?u ?r ?n ?  ?0 ?\; ?\C-m ?\} ?\C-p ?\C-p ?\C-i] 0 "%d"))
 
 ;; Give keybinding
 (global-set-key "\C-x\C-km" 'int-main)
+
+
+
+;; Python
+
+(defcustom python-shell-interpreter "python3"
+  "Default Python interpreter for shell."
+  :type 'string
+  :group 'python)
+
+
