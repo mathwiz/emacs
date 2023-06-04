9b59b1e8f8e58c81ef9e646d4d3e00f7c18760f1
Yohan Lee <yohanlee@yahoo.com>
2023-06-04 16:43:18 -0400

    init

--- .emacs.d/customizations/misc.el
+++ .emacs.d/customizations/misc.el
@@ -10,3 +10,18 @@
 
 ;; Go straight to scratch buffer on startup
 (setq inhibit-startup-message t)
+
+
+;; Language specific settings
+
+;; Octave customizations
+;; (setq auto-mode-alist
+;;       (cons '("\\.m$" . octave-mode) auto-mode-alist))
+      
+;; (add-hook 'octave-mode-hook
+;;           (lambda ()
+;;             (abbrev-mode 1)
+;;             (auto-fill-mode 1)
+;;             (if (eq window-system 'x)
+;;                 (font-lock-mode 1))))
+
